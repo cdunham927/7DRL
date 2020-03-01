f4fa16770356c7bff9f57be515e852a1db66fab5
Cole <cdunham927@gmail.com>
2020-02-29 20:48:35 -0800

    First push. Setup scripts: enemy stats and take damage, player stats and equipment/weapons, status effects for weapons and items. Etc etc etc dont break pls

---
+++ Assets/Scripts/Consumable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumable : Item
+{
+    public enum consumeType { potion }
+    public consumeType type;
+
+    public override void Use()
+    {
+        switch(type)
+        {
+
+        }
+    }
+}
